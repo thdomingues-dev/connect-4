0bf9160e2262d78c1bc219a543b48c1804743bca
Thales Domingues <thdomingues.dev@gmail.com>
2022-09-17 15:45:57 -0300

    feat: add diagonal win check

--- src/hooks/usePlayPiece.ts
+++ src/hooks/usePlayPiece.ts
@@ -3,6 +3,41 @@ import { useRecoilState } from "recoil";
 import { boardState, gameOverState, playerState } from "state";
 
 const testWin = (arr: number[]): boolean => /1{4}|2{4}/.test(arr.join(""));
+const testPositiveDiagonalWin = (arr: number[][], player: number): boolean => {
+  for (let i = 0; i <= arr.length - 1; i++) {
+    for (let j = 0; j <= arr[i].length - 1; j++) {
+      if (i + 3 >= arr.length) return false
+
+      if (arr[i][j] === player &&
+        arr[i+1][j+1] === player &&
+        arr[i+2][j+2] === player &&
+        arr[i+3][j+3] === player
+        ) {
+          return true
+      }
+    }
+  }
+
+  return false
+}
+
+const testNegativeDiagonalWin = (arr: number[][], player: number): boolean => {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = arr[i].length - 1; j >= 0 ; j--) {
+      if (i - 3 < 0) return false
+      
+      if (arr[i][j] === player &&
+        arr[i-1][j+1] === player &&
+        arr[i-2][j+2] === player &&
+        arr[i-3][j+3] === player
+        ) {
+          return true
+      }
+    }
+  }
+
+  return false
+}
 
 const usePlayPiece = () => {
   const [board, setBoard] = useRecoilState(boardState);
@@ -29,8 +64,9 @@ const usePlayPiece = () => {
 
     if (
       testWin(newBoard[col]) || // Did win vertically
-      testWin(newBoard.map((col) => col[row] || 0)) // Did win horizontally
-      // TODO: Did win diagonally
+      testWin(newBoard.map((col) => col[row] || 0)) || // Did win horizontally
+      testPositiveDiagonalWin(newBoard, player) || // Did win diagonally
+      testNegativeDiagonalWin(newBoard, player)
     ) {
       setGameOver(true);
     } else {
