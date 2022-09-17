b5e64cddcff10da275cd1ed9f3ad9f9645c47d78
Thales Domingues <thdomingues.dev@gmail.com>
2022-09-17 16:17:26 -0300

    feat: add persistence state between game sessions

--- src/state/game.ts
+++ src/state/game.ts
@@ -2,9 +2,30 @@ import { boardCols } from "const";
 import { atom } from "recoil";
 import { Board, Player } from "types";
 
+interface AtomEffect { 
+  setSelf: (args: any) => void
+  onSet: (args: any) => void
+};
+
+const localStorageEffect = (key: string) => ({setSelf, onSet}: AtomEffect) => {
+  const savedValue = localStorage.getItem(key)
+  if (savedValue != null) {
+    setSelf(JSON.parse(savedValue));
+  }
+
+  onSet((newValue: any, _:any, isReset: boolean) => {
+    isReset
+      ? localStorage.removeItem(key)
+      : localStorage.setItem(key, JSON.stringify(newValue));
+  });
+};
+
 export const boardState = atom<Board>({
   key: "boardState",
   default: Array(boardCols).fill([]),
+  effects: [
+    localStorageEffect('boardState')
+  ],
 });
 
 export const playerState = atom<Player>({
