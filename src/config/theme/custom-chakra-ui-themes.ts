c61768b59e0caa23a8d948f9832df1da433704b5
Thales Domingues <thdomingues.dev@gmail.com>
2022-09-17 21:19:44 -0300

    feat: adjust `Button`font-weight

--- src/config/theme/custom-chakra-ui-themes.ts
+++ src/config/theme/custom-chakra-ui-themes.ts
@@ -3,5 +3,13 @@ import { extendTheme } from "@chakra-ui/react";
 export const theme = extendTheme({
   fonts: {
     heading: `'Ubuntu', sans-serif`,
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        fontWeight: 'normal',
+        fontFamily: `'Ubuntu', sans-serif`,
+      },
+    }
   }
 });
